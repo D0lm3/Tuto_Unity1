57fcd2bd0cc2c3532539540d984206bed1b7dbd8
d0lm3nh1r <d0lm3nh1r@gmail.com>
2019-10-28 15:05:31 +0100

    Add the unity project + Objects + first script

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb ; //We use rb for rigidbody (to save time)
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello WOrld!"); //A debug message to see how the console works
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() //We use FixedUpdate in place of Update cause we mess with physics
+    {
+        rb.AddForce(0, 0, 1000 * Time.deltaTime); //We had some force to rigidbody to create movement
+    }
+}
