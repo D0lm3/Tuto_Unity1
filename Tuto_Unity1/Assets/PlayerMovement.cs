77076dd7111a3e0bd03225892251884cf18f5ee9
d0lm3nh1r <d0lm3nh1r@gmail.com>
2019-10-29 13:24:11 +0100

    Add player movement

--- Tuto_Unity1/Assets/PlayerMovement.cs
+++ Tuto_Unity1/Assets/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb ; //We use rb for rigidbody (to save time)
-    // Start is called before the first frame update
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    
     void Start()
     {
-        Debug.Log("Hello WOrld!"); //A debug message to see how the console works
+        Debug.Log("Hello World!"); //A debug message to see how the console works to show messages
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate() //We use FixedUpdate in place of Update cause we mess with physics
     {
-        rb.AddForce(0, 0, 1000 * Time.deltaTime); //We had some force to rigidbody to create movement
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //We had a forward force to rigidbody to create movement
+
+        if( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if( Input.GetKey("q"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
