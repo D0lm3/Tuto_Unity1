c208731d44257dfdbd5c07c4df874c0634cb8d68
d0lm3nh1r <d0lm3nh1r@gmail.com>
2019-11-06 13:53:27 +0100

    Add Score + Folders for scripts, Materials and Fonts

---
+++ Tuto_Unity1/Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player; // use to access the position (which can be found in the transform parameters) of the player
+    public Text scoreText; // cause the name of the component responsible of the view of the score is Text
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
