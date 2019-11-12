56e2eaafd565a0573a98964ccbda7226096bab59
d0lm3nh1r <d0lm3nh1r@gmail.com>
2019-11-12 14:28:29 +0100

    Add complete level screen + multiple levels + end screen

--- Tuto_Unity1/Assets/Scripts/GameManager.cs
+++ Tuto_Unity1/Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false; //use below to prevent the game to end multiple time (show GAME OVER ad nauseam)
+    public GameObject completeLevelUI; // reference to the complete level screen
+    public void CompleteLevel(){
+       completeLevelUI.SetActive(true);
+    }
   public void EndGame(){
       if(gameHasEnded == false){
       gameHasEnded = true; //prevent to end the game multiple time
